fcf579494ee74ab85524a1bc81b5f82e49f152e3
ant <antorrealbatapia@gamil.com>
2023-04-01 12:47:48 -0400

    user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 import indexrouter from "./routes/index.routes.js";
 import employeesRoutes from "./routes/employees.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import  { PORT } from './config.js'
 
 
@@ -8,10 +10,12 @@ import  { PORT } from './config.js'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use(indexrouter);
 app.use("/api", employeesRoutes);
+app.use("/api", userRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({
--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import {config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST= process.env.DB_HOST
-export const DB_PORT = process.env.DB_PORT
-export const DB_USER = process.env.DB_USER
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_HOST= process.env.DB_HOST || 'localhost'
+export const DB_PORT = process.env.DB_PORT || '3306'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'admin'
+export const DB_DATABASE = process.env.DB_DATABASE || 'companydb'
