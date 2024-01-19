10d69902ed62cd48149cd2e4f5d0244092e974fe
ant <antorrealbatapia@gamil.com>
2024-01-18 22:14:31 -0400

    insert de pedido

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -13,7 +13,7 @@ export const getPedidos = async (req, res) => {
   JOIN Clientes ON Pedidos.cliente_id = Clientes.cliente_id
   JOIN Usuarios ON Pedidos.usuario_id = Usuarios.usuario_id
   LEFT JOIN Repartidores ON Pedidos.repartidor_id = Repartidores.repartidor_id
-  ORDER BY Pedidos.pedido_id ASC;  -- Ordenar por pedido_id de forma ascendente
+  ORDER BY Pedidos.pedido_id DESC;  -- Ordenar por pedido_id de forma ascendente
   `)
       rows != [] ? res.json(rows) : res.json(false);
     } catch (error) {
@@ -21,4 +21,19 @@ export const getPedidos = async (req, res) => {
         message: "Somethin goes wrong",
       });
     }
+}
+
+export const postPedidos = async (req, res) =>{
+  const { cliente, repartidor, usuario } = req.body.params;
+  try{
+    console.log(req.body.params.cliente)
+    const [rows] = await pool.query(
+      "INSERT INTO Pedidos (cliente_id, usuario_id, repartidor_id, estatus_pedido) VALUES (?, ?, ?, ?)",[cliente, repartidor, usuario, "Nuevo"]
+  )
+    res.json("Pedido creado exitosamente")
+  }catch (error){
+    return res.status(500).json({
+      message: "Somethin goes wrong",
+    });
+  }
 }
--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getPedidos } from "../controllers/pedidos.controllers.js";
+import { getPedidos, postPedidos } from "../controllers/pedidos.controllers.js";
 const router = Router();
 
 router.get("/pedidos", getPedidos);
+router.post("/pedidos", postPedidos);
 
 export default router;
