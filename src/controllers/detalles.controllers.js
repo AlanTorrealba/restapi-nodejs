ad5f7f74280cff45f367421af46b6cf2851de07f
ant <antorrealbatapia@gamil.com>
2024-05-26 16:05:09 -0400

    Detalles del pedido

--- src/controllers/detalles.controllers.js
+++ src/controllers/detalles.controllers.js
@@ -1,17 +1,16 @@
 import { pool } from "../db.js";
 export const getDetalles = async (req, res) => {
-    const id = req.params.id
-   
-  try {
-    if ( id != 0 ){
+  const id = req.params.id;
 
-        const [rows] = await pool.query(`
-        SELECT d.detalle_id, p.nombre, d.cantidad, d.precio_unitario, d.pedido_id  FROM detallespedido d left join
+  try {
+    const [rows] = await pool.query(
+      `
+        SELECT d.detalle_id, p.nombre, d.cantidad, d.estatus, d.precio_unitario, d.pedido_id  FROM detallespedido d left join
      productos p on d.producto_id = p.producto_id where pedido_id = ?;
-      `, [id]);
-      console.log(json(rows))
-        rows != [] ? res.json(rows) : res.json(false);
-    }
+      `,
+      [id]
+    );
+    rows != [] ? res.json(rows) : res.json(false);
   } catch (error) {
     return res.status(500).json({
       message: "Somethin goes wrong",
@@ -19,12 +18,19 @@ export const getDetalles = async (req, res) => {
   }
 };
 export const postDetalles = async (req, res) => {
-  const { cliente, repartidor, usuario } = req.body.params;
+  const { pedidoId, products, cantidad } = req.body.params;
+  console.log(pedidoId, products, cantidad);
+
   try {
+    // Insertar el detalle del pedido con subconsulta para obtener el precio_unitario
     const [rows] = await pool.query(
-      "INSERT INTO Pedidos (cliente_id, usuario_id, repartidor_id, estatus_pedido) VALUES (?, ?, ?, ?)",
-      [cliente, usuario, repartidor, "Nuevo"]
+      `
+      INSERT INTO detallespedido (pedido_id, producto_id, cantidad, estatus, precio_unitario)
+      VALUES (?, ?, ?, ?, (SELECT precio FROM productos WHERE producto_id = ?))
+      `,
+      [pedidoId, products, cantidad, 1, products]
     );
+
     res.status(200).json({
       success: true,
       data: rows,
@@ -33,7 +39,7 @@ export const postDetalles = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: "Somethin goes wrong",
+      message: "Something goes wrong",
     });
   }
 };
